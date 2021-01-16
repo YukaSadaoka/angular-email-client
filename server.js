6c1889e8b5992e25792e4b6766123e89ed91b149
Yuka Sadaoka <yukasadaoka@outlook.com>
2021-01-15 21:44:56 -0500

    Changed app name

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/email-client'));
+app.use(express.static(__dirname + '/dist/EmailClient'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/email-client/index.html'));
+res.sendFile(path.join(__dirname+'/dist/EmailClient/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
